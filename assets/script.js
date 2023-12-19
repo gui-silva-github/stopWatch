d21c0c13aa0d06766686be68c6bb02dd146ebc6e
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2023-12-19 17:51:48 -0300

    Recursos (CSS e Javascript) do Cronômetro

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+let [seconds, minutes, hours] = [0,0,0];
+
+let display = document.querySelector('#display');
+
+let timer = null;
+
+function watchStop(){
+    seconds++;
+    if (seconds==60){
+        seconds=0;
+        minutes++;
+        if (minutes==60){
+            minutes=0;
+            hours++;
+        }
+    }
+    display.innerHTML = fixeZero(hours) + ":" + fixeZero(minutes) + ":" + fixeZero(seconds);
+};
+
+function fixeZero(time){
+    return time < 10 ? `0${time}` : `${time}`;
+};   
+
+function startWatch(){
+    if (timer !== null){
+        clearInterval(timer);
+    }
+    timer = setInterval(watchStop, 1000);
+};
+
+function stopWatch(){
+    clearInterval(timer);
+};
+
+function resetWatch(){
+    clearInterval(timer);
+    [seconds, minutes, hours] = [0,0,0];
+    display.innerHTML = "00:00:00";
+}
